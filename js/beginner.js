193b3809be22a8218e2792b0148131177a7c90ce
Karen Rustad <karen.rustad@gmail.com>
2013-10-14 17:05:40 -0700

    Tried to follow part 2 of the tutorial, but there's no damn audio element

--- js/beginner.js
+++ js/beginner.js
@@ -1,1 +1,34 @@
 var app = angular.module('myApp', []);
+
+/* dumb sample stuff */
+
+app.run(function($rootScope) {
+  $rootScope.name = "Karen Rustad";
+});
+
+app.controller('MyController', function($scope) {
+  $scope.person = {
+    name: "Ari Lerner"
+  };
+});
+
+app.controller('ParentController', function($scope) {
+  $scope.person = {greeted: false};
+});
+
+app.controller('ChildController', function($scope) {
+  $scope.sayHello = function() {
+    $scope.person.greeted = true;
+  }
+});
+
+/* end dumb sample stuff */
+
+app.controller('PlayerController', ['$scope', function($scope) {
+    $scope.playing = false;
+    $scope.audio = document.createElement('audio');
+    $scope.audio.src = '/media/halfwaythere.mp3';
+}]);
+
+app.controller('RelatedController', ['$scope', function($scope) {
+}]);
