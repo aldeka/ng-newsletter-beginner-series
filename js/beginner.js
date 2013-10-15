9c0abff8d1919869c1c6c5ddd17170635408a578
Karen Rustad <karen.rustad@gmail.com>
2013-10-14 17:47:41 -0700

    following along, NPR api key

--- js/beginner.js
+++ js/beginner.js
@@ -1,12 +1,30 @@
 var app = angular.module('myApp', []);
 
-app.controller('PlayerController', ['$scope', function($scope) {
-  var audio = document.createElement('audio');
-  $scope.audio = audio;
+// app.controller('PlayerController', ['$scope', function($scope) {
+//   var audio = document.createElement('audio');
+//   $scope.audio = audio;
 
-  audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-  audio.play();
-}]);
+//   audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
+//   audio.play();
+// }]);
+
+var apiKey = 'MDExMDMwMzU0MDEzNjMwNzc4MDcwMWQ5OA001',
+    nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+
+app.controller('PlayerController', function($scope, $http) {
+  // Hidden our previous section's content
+  // construct our http request
+  $http({
+    method: 'JSONP',
+    url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+  }).success(function(data, status) {
+    // Store the list of stories on the scope
+    // from the NPR API response object (described above)
+    $scope.programs = data.list.story;
+  }).error(function(data, status) {
+    // Some error occurred
+  });
+});
 
 app.controller('RelatedController', ['$scope', function($scope) {
 }]);
